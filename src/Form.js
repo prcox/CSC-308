fdd15b773987c70a5b8916a06a966c49963ac58a
prcox <prcox@calpoly.edu>
2021-01-08 19:53:18 -0800

    Assignment 2 Final State

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-function Form() {   
+function Form(props){   
    const [person, setPerson] = useState([
       {  
          name: '',
@@ -18,7 +18,12 @@ function Form() {
           setPerson(
               {name: value, job: person['job']}
            );   
-    }   
+    }  
+    
+    function submitForm() {
+        props.handleSubmit(person);
+        setPerson({name: '', job: ''});
+      }
 
    return (
     <form>
@@ -36,6 +41,7 @@ function Form() {
         id="job"
         value={person.job}
         onChange={handleChange} />
+        <input type="button" value="Submit" onClick={submitForm} />
     </form>
     ); 
 }
